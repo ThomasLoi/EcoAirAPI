d29151a2e68560a464c9d72c4fa36fa4c884e4fc
LOI Kong Leong Thomas <loith@champ.aero>
2022-05-21 15:36:21 +0200

    Revert "revert initial commit"
    This reverts commit 7ff9879d01ce7a149983cc34b41014151ac97318.

---
+++ src/test/java/com/hackathon/ecoair/EcoairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.ecoair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
